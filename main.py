4d75b93a3d96012d31616e8a2614751f5c55f220
isaias <familynataias@gmail.com>
2024-10-01 21:09:57 -0300

    foi o que deu

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+print('olá seja bem vindo ao nosso app LIXO da SEDUC ')
+gmail = input('Digite o Gmail para o cadastro: ')
+senha = input('Digite uma senha para a sua conta: ')
+def teste_numero():
+    senha.isdigit
+    return True
+    return False
+def teste_letra():
+    senha.isalpha
+    return True
+    return False
+
+if teste_numero():
+    
+ if teste_letra():
+    print('X')
+        
+if teste_numero() == False:
+        
+ if teste_letra() == False:
+    print('y')
+#def caracteres_NL():
